e8c580584ffa8354ad2f31222319664c36cf6d2c
Rob11005 <langerobin05@gmail.com>
2025-11-06 16:28:22 +0100

    Item Description

--- Assets/Scripts/BuildingOption.cs
+++ Assets/Scripts/BuildingOption.cs
@@ -1,4 +1,5 @@
 using Microsoft.Unity.VisualStudio.Editor;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,9 +7,19 @@ public class BuildingOption : MonoBehaviour
 {
     public GameObject buildingInfo;
     public Button button;
+    public BuildingsData datas;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI prodText;
+    private Bâtiment bat;
 
     void Awake()
     {
+        bat = GetComponent<Bâtiment>();
+        nameText.text = datas.buildingName;
+        foreach(var r in datas.levels[bat.b_level].production)
+        {
+            prodText.text += $"{r.resourceName} : {r.amount}\n";
+        }
         button.onClick.AddListener(OnClicked);
     }
     public void OnSelected()
